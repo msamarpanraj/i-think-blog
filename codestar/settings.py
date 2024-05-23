1db5bcf07c143d5e9f60869297968c486c84527e
Samarpan Mathangi <samarpanraaz@gmail.com>
2024-05-23 21:37:01 +0000

    readies code for deploy

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7h&ydtiwny5fmyl7@y4^pmfe)1_)i$9+aky+(21s9t&gqdsx_*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-msamarpanraj-ithinkblog-qoz2pdycmnp.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-msamarpanraj-ithinkblog-qoz2pdycmnp.ws.codeinstitute-ide.net','.herokuapp.com'
+]
 
 
 # Application definition
